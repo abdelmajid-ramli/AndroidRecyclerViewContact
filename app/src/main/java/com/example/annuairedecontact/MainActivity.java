9f0b0d59985f42206ddfc82692bc99f64814a1fc
abdelmajid-ramli <abdalmajidardesings@gmail.com>
2022-04-15 11:50:02 +0000

    first commit

---
+++ app/src/main/java/com/example/annuairedecontact/MainActivity.java
@@ -0,0 +1,142 @@
+package com.example.annuairedecontact;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.DragEvent;
+import android.view.View;
+
+import androidx.appcompat.widget.SearchView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.annuairedecontact.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private int REQUEST_CALL;
+    private ActivityMainBinding binding;
+    RecyclerView recyclerView;
+
+    List<Contact> dataList=new ArrayList<>();
+    LinearLayoutManager linearLayoutManager;
+    RoomDB database;
+    MainAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myInent=new Intent(view.getContext(),MainActivity2.class);
+                startActivity(myInent);
+            }
+        });
+        recyclerView=findViewById(R.id.recyclerV);
+
+        // initialize database
+        database=RoomDB.getInstance(this);
+        // store database value in data list
+
+        dataList=database.contactDao().getAll();
+
+        //Initialize linear layout manager
+        linearLayoutManager=new LinearLayoutManager(this);
+
+        // Set layout manager
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        // Initialize adapter
+        adapter=new MainAdapter(MainActivity.this,dataList);
+
+        // set adapter
+
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.setOnDragListener(new View.OnDragListener() {
+
+            @Override
+            public boolean onDrag(View view, DragEvent dragEvent) {
+                phoneCall("45454545");
+                return false;
+            }
+        });
+    }
+
+    private void phoneCall(String num) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.CALL_PHONE,num}, REQUEST_CALL);
+        } else {
+            String phoneNumber = "tel:" + num;
+            startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(phoneNumber)));
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String[] permissions,int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CALL) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //Contact contact = new Contact();
+                //contact.setPhoneNumber(permissions[1]);
+                phoneCall("45454545");
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    public void call(View view){
+        phoneCall("54545454");
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem search = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) search.getActionView();
+        searchView.setQueryHint("enter contact name");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.searching(s);
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
